cb5677ddb6269a98b4ff1b359150014d87365f20
vishalgupta941994 <vishalgupta19941994@gmail.com>
2019-12-02 23:06:21 +0530

    commit new data

--- app.js
+++ app.js
@@ -8,10 +8,41 @@ then((data)=>{
     console.log("  error while connecting to mongodb ");
  });
 
-// 	(err,data)=>{
-// 	if(err){
-//         console.log("error   ",err);
-// 	}else{ 
-//         console.log("data ",data);
-// 	}
-// });
+//schema to shape of documents in mongoDB.
+//collection in  mongodb is like a tble in RdB
+//in this courses collection document is like row in rdb
+
+//schema is used to define the shape of document in mongodb collection
+
+var courseSchema3=new mongoose.Schema({
+   name:{type:String},
+   author:{type:String},
+   tags:[String],
+   date:{type:String,default:Date.now},
+   isPublished: Boolean      	
+})
+
+//String Number Date Buffer Boolean ObjectId Array
+
+//Classes,Object
+//to create object of the class we need to compile schema to model
+
+
+const Course = mongoose.model("Course3",courseSchema3);
+
+void courseSave(){
+const course = new Course({
+	name:"Angular Course",
+	author:"Mosh",
+	tags:["angular","front"],
+	isPublished:true
+});
+
+course.save().then((data)=>{
+	console.log("new docuemtn added",data)
+},(err)=>{console.log("error while saving document")})
+
+
+}
+
+courseSave();
